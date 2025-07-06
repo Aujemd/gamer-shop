b224cc25f589dc262d6c5e424bc1b3771b7e77ae
Johan <joenmodu99@gmail.com>
2025-07-06 15:52:10 -0400

    WIP on main

--- src/shared/components/molecules/ml-product-card.tsx
+++ src/shared/components/molecules/ml-product-card.tsx
@@ -29,11 +29,11 @@ export default function MlProductCard(props: MlProductCard) {
             className={`p-6 space-y-5 border-half border-stroke-secondary rounded-4xl font-archivo ${className}`}
             {...rest}
         >
-            <div className="relative h-[15rem] overflow-hidden rounded-t-2xl">
+            <div className="relative h-60">
                 <Image
                     src={image}
                     alt={name}
-                    className="object-cover"
+                    className="object-cover rounded-t-2xl"
                     fill
                     data-testid={`ml-product-card-image-test-id-${id}`}
                     sizes="(max-width: 768px) 40vw, (max-width: 1200px) 33vw"
